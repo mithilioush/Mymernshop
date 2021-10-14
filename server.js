49db34fa87731d0be47568e7cbfc0d614f8d93e2
Navimaurya <mauryanavis@gmail.com>
2021-10-14 10:50:21 +0530

    Buid app

--- server.js
+++ server.js
@@ -29,6 +29,9 @@ if (process.env.NODE_ENV !== 'production') {
         })
     })
 
+    app.get('/', (req, res) => {
+        res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
+    })
     app.get('*', (req, res) => {
         res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
     })
