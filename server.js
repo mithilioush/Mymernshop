e40de27cb2d263501072070c16d5df54df93b444
navimaurya <mauryanavis@gmail.com>
2021-12-11 16:58:46 +0530

    Build

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ if (process.env.NODE_ENV !== 'production') {
     app.get('/', (req, res) => {
         res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
     })
-    app.get('*', (req, res) => {
+    app.use('*', (req, res) => {
         res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
     })
 }
